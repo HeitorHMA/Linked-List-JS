d36a4f2ee07aaafac6ede7966373fcc0ad166796
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-04-30 13:52:10 -0300

    Create List class

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 //Create Node Class//
+
 class listNode {
     constructor(data){
         this.data = data;
         this.next = null;
     }
 }
+
+//Create List Class//
+
 class linkedList{
     constructor(head = null){
         this.head = head;
@@ -14,17 +18,7 @@ let node5 = new listNode(5);
 let node2 = new listNode(2);
 node5.next = node2;
 
-
-
-
-
-
-
-
-
-
-
-
+//
 
 
 
